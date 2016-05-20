c98266a399e273d59c1336e8d7e43f707c0981a5
yash2710 <13bce123@nirmauni.ac.in>
2016-05-20 15:44:22 +0530

    list click

---
+++ app/src/main/java/me/yashtrivedi/ideal_succotash/ClickListener.java
@@ -0,0 +1,11 @@
+package me.yashtrivedi.ideal_succotash;
+
+import android.view.View;
+
+/**
+ * Created by yashtrivedi on 20/05/16.
+ */
+interface ClickListener{
+    void onClick(View view, int position);
+    void onLongClick(View view, int position);
+}
