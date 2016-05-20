4bcc294e56c03136622dd5d650f8aa8de28f846a
yash2710 <13bce123@nirmauni.ac.in>
2016-05-20 15:22:59 +0530

    list click

--- app/src/main/java/me/yashtrivedi/ideal_succotash/ListFragment.java
+++ app/src/main/java/me/yashtrivedi/ideal_succotash/ListFragment.java
@@ -1,20 +1,29 @@
 package me.yashtrivedi.ideal_succotash;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ListFragment extends Fragment {
+public class ListFragment extends Fragment implements ClickListener{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
     private OnFragmentInteractionListener mListener;
     private RecyclerView recyclerView;
+
     public ListFragment() {
         // Required empty public constructor
     }
@@ -25,21 +34,86 @@ public class ListFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_list, container, false);
         recyclerView = (RecyclerView) v.findViewById(R.id.list);
-        recyclerView.setAdapter(new RViewAdapter(getContext()));
+        RViewAdapter adapter = new RViewAdapter(getContext());
+//        adapter.setList();
+        recyclerView.setAdapter(adapter);
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getContext(),recyclerView,this));
         return v;
     }
 
+    @Override
+    public void onClick(View view, int position) {
+        //Dialog code
+        AlertDialog dialog = new AlertDialog.Builder(getContext()).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                NotificationManager notificationManager = (NotificationManager) getContext().getSystemService(Context.NOTIFICATION_SERVICE);
+                Intent intent = new Intent(getContext(),MainActivity.class);
+                PendingIntent pendingIntent;
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(getContext())
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setTicker("Requesting Ride")
+                        .setOngoing(true)
+                        .addAction(new NotificationCompat.Action(R.drawable.ic_close_black_24dp,"Cancel",null));
+                notificationManager.notify(13123,builder.build());
+                //notify user
+            }
+        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        }).create();
+        dialog.show();
+    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+    @Override
+    public void onLongClick(View view, int position) {
+
+    }
+
+    class RecyclerTouchListener implements RecyclerView.OnItemTouchListener{
+
+        private GestureDetector gestureDetector;
+        private ClickListener clickListener;
+
+        public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final ClickListener clickListener){
+            this.clickListener=clickListener;
+            gestureDetector = new GestureDetector(context,new GestureDetector.SimpleOnGestureListener(){
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                    if (child != null && clickListener != null)
+                        clickListener.onLongClick(child, recyclerView.getChildLayoutPosition(child));
+                }
+
+            });
+        }
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+            View child = rv.findChildViewUnder(e.getX(), e.getY());
+            if (child != null && clickListener != null && gestureDetector.onTouchEvent(e))
+                clickListener.onClick(child, rv.getChildLayoutPosition(child));
+
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+        }
+    }
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
--- app/src/main/java/me/yashtrivedi/ideal_succotash/MainActivity.java
+++ app/src/main/java/me/yashtrivedi/ideal_succotash/MainActivity.java
@@ -15,8 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction().add(R.id.container,new ListFragment()).commit();
 
     }
+
     public void showRequestForm(View view){
         DialogFragment dialogFragment = ShowRequestFormFragment.newInstance();
         dialogFragment.show(getSupportFragmentManager(), "ShowRequestFormFragment");
