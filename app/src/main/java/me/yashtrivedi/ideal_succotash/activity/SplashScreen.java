d8da2e77a74e8755a8b5cba07f68bfcc19501d01
yash2710 <13bce123@nirmauni.ac.in>
2016-05-28 17:23:58 +0530

    nullpointer

--- app/src/main/java/me/yashtrivedi/ideal_succotash/activity/SplashScreen.java
+++ app/src/main/java/me/yashtrivedi/ideal_succotash/activity/SplashScreen.java
@@ -20,7 +20,7 @@ public class SplashScreen extends AwesomeSplash {
     @Override
     public void initSplash(ConfigSplash configSplash) {
         getSupportActionBar().hide();
-        configSplash.setBackgroundColor(R.color.colorPrimary);
+        configSplash.setBackgroundColor(R.color.colorAccent);
         configSplash.setAnimCircularRevealDuration(1000); //int ms
         configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
         configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
@@ -30,9 +30,9 @@ public class SplashScreen extends AwesomeSplash {
         configSplash.setOriginalWidth(475); //in relation to your svg (path) resource
         configSplash.setAnimPathStrokeDrawingDuration(3000);
         configSplash.setPathSplashStrokeSize(3); //I advise value be <5
-        configSplash.setPathSplashStrokeColor(R.color.colorAccent); //any color you want form colors.xml
+        configSplash.setPathSplashStrokeColor(R.color.colorPrimaryDark); //any color you want form colors.xml
         configSplash.setAnimPathFillingDuration(2000);
-        configSplash.setPathSplashFillColor(R.color.strokeColor); //path object filling color
+        configSplash.setPathSplashFillColor(R.color.colorPrimary); //path object filling color
 
         configSplash.setTitleSplash("");
 
@@ -44,5 +44,6 @@ public class SplashScreen extends AwesomeSplash {
 
         Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
         startActivity(intent);
+        finish();
     }
 }
